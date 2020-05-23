9fa9fc438324711052511eee1c13a720dd838c5f
Skullcr1 <kornelismak@gmail.com>
2020-05-23 14:12:30 +0300

    implemented CreateNewRoom functionality

--- MyApplication/app/src/main/java/com/example/myapplication/view/CreateRoomActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/view/CreateRoomActivity.java
@@ -10,10 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -23,20 +26,26 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CreateRoomActivity extends AppCompatActivity {
 
-    EditText editText;
+    EditText playerNameInput;
+    EditText cityNameInput;
+    EditText mafiaAmountInput;
+    EditText civilAmountInput;
     TextView textView;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
     String name;
 
     Button enterData;
-    private RequestQueue mRequestQueue;
+
     private StringRequest mStringRequest;
     List<String> playerNicks = new ArrayList<String>();
-
-
 
 
     @Override
@@ -44,47 +53,92 @@ public class CreateRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_room);
 
-        textView = findViewById(R.id.textView2);
-        editText = findViewById(R.id.editText);
-        enterData = findViewById(R.id.button);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+        textView = findViewById(R.id.textView5);
+        mafiaAmountInput = findViewById(R.id.MafiaAmountInput);
+        civilAmountInput = findViewById(R.id.CivilAmountInput);
+        playerNameInput = findViewById(R.id.PlayerNameInput);
+        cityNameInput = findViewById(R.id.CityNameInput);
+        enterData = findViewById(R.id.CreateRoomButton);
+
         enterData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //sendAndRequestResponse();
-                requestPlayerData();
+                //requestPlayerData();
+                //sendWorkPostRequest();
+               HttpPOSTRequestWithParameters();
 
             }
         });
     }
-        private void postDataToServer(){
-            mRequestQueue = Volley.newRequestQueue(this);
-//            mStringRequest = new StringRequest(Request.Method.POST, url, new )
-        }
 
-        //
-        private void sendAndRequestResponse() {
-            //RequestQueue initialized
-            String url = "http://10.0.2.2:63439/api/GetPlayers";
+    public void HttpPOSTRequestWithParameters() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:63439/api/CreateNewRoom";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
 
-            mRequestQueue = Volley.newRequestQueue(this);
-
-            //String Request initialized
-            mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-
-                    Toast.makeText(getApplicationContext(),"Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR","error => "+error.toString());
+                    }
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
+        ) {
 
-                    Log.i("test","Error :" + error.toString());
-                }
-            });
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                //..add other headers
+                return params;
+            }
 
-            mRequestQueue.add(mStringRequest);
-        }
+
+            // this is the relevant method
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("name", cityNameInput.getText().toString());
+                // volley will escape this for you
+                //params.put("randomFieldFilledWithAwkwardCharacters", "{{%stuffToBe Escaped/");
+                params.put("playerName", playerNameInput.getText().toString());
+                params.put("civilAmount", civilAmountInput.getText().toString());
+                params.put("mafiaAmount", mafiaAmountInput.getText().toString());
+
+                return params;
+            }
+
+            @Override
+            protected VolleyError parseNetworkError(VolleyError response) {
+                try {
+
+                    String json = new String(response.networkResponse.data, HttpHeaderParser.parseCharset(response.networkResponse.headers));
+                    Log.e("tag", "reponse error = " + json);
+                }catch (Exception e){}
+                return super.parseNetworkError(response);
+            }
+
+
+        };
+        postRequest.setRetryPolicy(new DefaultRetryPolicy(
+                30000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(postRequest);
+    }
+
         //gets players nicks and then adds them to arraylist
     public void requestPlayerData() {
         String url = "http://10.0.2.2:63439/api/GetPlayers";
@@ -103,6 +157,7 @@ public class CreateRoomActivity extends AppCompatActivity {
                                 playerNicks.add(name);
                             }
                         }
+                        Toast.makeText(CreateRoomActivity.this, response+name+"", Toast.LENGTH_SHORT).show();
                     }
                 }catch(Exception e){
                     e.printStackTrace();
@@ -114,11 +169,7 @@ public class CreateRoomActivity extends AppCompatActivity {
                 Log.e("LOG", error.toString());
             }
         });
-
         queue.add(jsonArrayRequest);
     }
-
-
-
 }
 
